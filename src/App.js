79d2bbbb560d506b93ffb3977062d7fc437e2785
Hrishita。 <70261156+hri28@users.noreply.github.com>
2021-09-21 23:56:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Components/Header';
+import Features from './Components/Features';
+import Info from './Components/Info';
+import FooterSection from './Components/FooterSection';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Features />
+      <Info />
+      <FooterSection />
+    </div>
+  );
+}
+
+export default App;
